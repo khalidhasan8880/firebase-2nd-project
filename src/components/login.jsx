fc46aac012c50645f3912ef38613f424f6c616ac
khalidhasan8880 <khalidhasan8880@gmail.com>
2023-04-17 09:13:02 +0600

    added reset password functionality

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,33 +1,44 @@
-import React, { useState } from 'react';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { useRef, useState } from 'react';
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../firebase.init';
 import { Link } from 'react-router-dom';
 
+
 const auth = getAuth(app)
 
-
 const Login = () => {
-    const [error, setError]= useState('')
-    const [success, setSuccess]= useState('')
+    const emailRef = useRef()
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
 
-    const formHandler =(event)=>{
+    const formHandler = (event) => {
         event.preventDefault()
         const email = event.target.email.value
         const password = event.target.password.value
-        console.log(email,password)
-    
+        console.log(email, password)
 
-        signInWithEmailAndPassword(auth,email,password)
-        .then(res=> {
-            const loggedUser = res.user
-            console.log(loggedUser);
-            setSuccess('user logged successfully')
-            setError('')
-        })
-        .then(error=>{
-            console.log(error);
-        })
-       
+
+
+    }
+
+    const resetPasswordHandler = () => {
+        const email = emailRef.current.value
+       if (!email) {
+        alert('please give your email address')
+        return
+       }
+
+        sendPasswordResetEmail(auth, email)
+            .then(() => {
+                // Password reset email sent!
+                alert('please check your email')
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorMessage);
+                // ..
+            });
     }
     return (
         <div>
@@ -38,6 +49,7 @@ const Login = () => {
                         Email
                     </label>
                     <input
+                        ref={emailRef}
                         name='email'
                         type="email"
                         className="form-control"
@@ -57,13 +69,13 @@ const Login = () => {
                         id="password"
                         placeholder="Enter password"
                         required
-                         
+
                     />
                     <p className='text-danger'>{error}</p>
                     <p className='text-success'>{success}</p>
                 </div>
                 <p>No account ? please <Link to="/signup">sign up</Link></p>
-                <p>Forget password ? {}</p>
+                <p>Forget password ? <button onClick={resetPasswordHandler} className='btn btn-link'>Reset password</button></p>
                 <button type="submit" className="btn btn-primary">
                     Login
                 </button>
